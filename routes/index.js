1d9d7452ea89668ba275d4facc409bab1a3c72bf
MishaelChL <jose.mishael.chile@gmail.com>
2021-04-16 14:40:56 -0400

    adicionando la carpeta public e iniciando el master page

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,6 @@ module.exports = function () {
   //ruta para el home
   router.get("/", proyectosController.proyectosHome);
   
-  router.get("/nosotros", (req, res) => {
-    res.render('nosotros');
-  })
-  
   
   
   return router;
