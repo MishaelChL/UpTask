38286037a6322e9b5f5ebb48ee9a2c0cd4864ba6
MishaelChL <jose.mishael.chile@gmail.com>
2021-04-17 22:29:03 -0400

    enviando una peticion con axios para eliminar un proyecto

--- public/js/modulos/proyectos.js
+++ public/js/modulos/proyectos.js
@@ -2,24 +2,43 @@ import Swal from "sweetalert2";
 import axios from "axios";
 
 const btnEliminar = document.querySelector("#eliminar-proyecto");
-btnEliminar.addEventListener("click", () => {
-  Swal.fire({
-    title: "Deseas borrar este Proyecto?",
-    text: "Un proyecto eliminado no se puede recuperar!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Si, borrar",
-    cancelButtonText: "No, cancelar",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire("Eliminado!", "Tu proyecto se ha eliminado", "success");
 
-      //redireccionar al inicio
-      setTimeout(() => {
-          window.location.href = "/";
-      }, 3000);
-    }
-  });
-});
+if(btnEliminar){
+    btnEliminar.addEventListener("click", event => {
+        const urlProyecto = event.target.dataset.proyectoUrl;
+        // console.log(urlProyecto);
+        Swal.fire({
+          title: "Deseas borrar este Proyecto?",
+          text: "Un proyecto eliminado no se puede recuperar!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Si, borrar",
+          cancelButtonText: "No, cancelar",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            //enviar peticion a axios
+            const url = `${location.origin}/proyectos/${urlProyecto}`;
+            // console.log(url);
+            
+            axios.delete(url, { params: urlProyecto })
+                .then(function(respuesta){
+                    console.log(respuesta);
+                });
+
+                return;
+
+            Swal.fire("Eliminado!", "Tu proyecto se ha eliminado", "success");
+      
+            //redireccionar al inicio
+            setTimeout(() => {
+                window.location.href = "/";
+            }, 3000);
+          }
+        });
+    });
+}
+
+export default btnEliminar;
+
