b2678a6cfd1dcec1012f8117cfe38ed57d34afad
MishaelChL <jose.mishael.chile@gmail.com>
2021-04-22 14:16:28 -0400

    creando variables de entorno para la base de datos

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 const Sequelize  = require('sequelize');
 
-const db = new Sequelize('uptasknode', 'postgres', '0000', {
-  host: 'localhost',
-  dialect: 'postgres',
+//extraer valores de variables.env
+require("dotenv").config({path: "variables.env"})
+
+const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
+  host: process.env.BD_HOST,
+  dialect: "postgres",
+  port: process.env.BD_PORT,
   define: {
     timestamps: false
   },
